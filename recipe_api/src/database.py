0d15c5821faa64637e5cc3e8fb9612f52482c959
Peeeeeeeeei <p39757323@gmail.com>
2026-06-02 21:50:44 +0800

    修正資料庫連線，切換至 recipe_db

--- recipe_api/src/database.py
+++ recipe_api/src/database.py
@@ -1,9 +1,9 @@
 import databases
 import sqlalchemy
 
-# 🚀 終極核彈版：正確的小寫 l + 預設資料庫 defaultdb
-SYNC_DB_URL = "mysql+pymysql://avnadmin:AVNS_0VdpgfC676hdRVeecM3@mysql-2c8679e9-p39757323-76b2.l.aivencloud.com:10217/defaultdb"
-ASYNC_DB_URL = "mysql+aiomysql://avnadmin:AVNS_0VdpgfC676hdRVeecM3@mysql-2c8679e9-p39757323-76b2.l.aivencloud.com:10217/defaultdb"
+# 🚀 終極核彈版：已經把空房間 defaultdb 換成真正有 300 多道食譜的 recipe_db 囉！
+SYNC_DB_URL = "mysql+pymysql://avnadmin:AVNS_0VdpgfC676hdRVeecM3@mysql-2c8679e9-p39757323-76b2.l.aivencloud.com:10217/recipe_db"
+ASYNC_DB_URL = "mysql+aiomysql://avnadmin:AVNS_0VdpgfC676hdRVeecM3@mysql-2c8679e9-p39757323-76b2.l.aivencloud.com:10217/recipe_db"
 
 database = databases.Database(ASYNC_DB_URL)
 
