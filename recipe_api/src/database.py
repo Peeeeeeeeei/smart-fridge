491fb44d6aa6c1fa29decfd0a89ba196e1d894cb
Peeeeeeeeei <p39757323@gmail.com>
2026-06-02 12:01:54 +0800

    修正 Aiven 網址為正確的字母 a

--- recipe_api/src/database.py
+++ recipe_api/src/database.py
@@ -1,14 +1,13 @@
 import databases
 import sqlalchemy
-from src.config import settings
 
-# 🚀 給 databases 用的非同步連線 (維持原樣)
-database = databases.Database(settings.DATABASE_URL)
+# 🚀 把 76b2 後面的字母改成 a ！！！
+SYNC_DB_URL = "mysql+pymysql://avnadmin:AVNS_0VdpgfC676hdRVeecM3@mysql-2c8679e9-p39757323-76b2.a.aivencloud.com:10217/recipe_db"
+ASYNC_DB_URL = "mysql+aiomysql://avnadmin:AVNS_0VdpgfC676hdRVeecM3@mysql-2c8679e9-p39757323-76b2.a.aivencloud.com:10217/recipe_db"
 
-# 💡 破案關鍵：將 +aiomysql 替換成 +pymysql，讓 sqlalchemy 可以使用同步引擎建立資料表
-sync_db_url = settings.DATABASE_URL.replace("+aiomysql", "+pymysql")
+database = databases.Database(ASYNC_DB_URL)
 
 engine = sqlalchemy.create_engine(
-    sync_db_url,
-    connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}
+    SYNC_DB_URL,
+    connect_args={}
 )
