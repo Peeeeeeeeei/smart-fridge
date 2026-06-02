c1025cb61ce2f93ac2f69e645631c2cf4d67b178
Peeeeeeeeei <p39757323@gmail.com>
2026-06-02 12:09:55 +0800

    修正為小寫 l 並更改為預設資料庫 defaultdb

--- recipe_api/src/database.py
+++ recipe_api/src/database.py
@@ -1,9 +1,9 @@
 import databases
 import sqlalchemy
 
-# 🚀 把 76b2 後面的字母改成 a ！！！
-SYNC_DB_URL = "mysql+pymysql://avnadmin:AVNS_0VdpgfC676hdRVeecM3@mysql-2c8679e9-p39757323-76b2.a.aivencloud.com:10217/recipe_db"
-ASYNC_DB_URL = "mysql+aiomysql://avnadmin:AVNS_0VdpgfC676hdRVeecM3@mysql-2c8679e9-p39757323-76b2.a.aivencloud.com:10217/recipe_db"
+# 🚀 終極核彈版：正確的小寫 l + 預設資料庫 defaultdb
+SYNC_DB_URL = "mysql+pymysql://avnadmin:AVNS_0VdpgfC676hdRVeecM3@mysql-2c8679e9-p39757323-76b2.l.aivencloud.com:10217/defaultdb"
+ASYNC_DB_URL = "mysql+aiomysql://avnadmin:AVNS_0VdpgfC676hdRVeecM3@mysql-2c8679e9-p39757323-76b2.l.aivencloud.com:10217/defaultdb"
 
 database = databases.Database(ASYNC_DB_URL)
 
